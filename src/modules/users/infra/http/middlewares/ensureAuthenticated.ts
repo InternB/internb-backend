ee72d1a0e038d0a31134856d0640665c635cf4bd
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-05-22 12:00:59 -0300

    ensureAuthenticated middleware ready

---
+++ src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,37 @@
+import { verify } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+import authConfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const { authorization } = request.headers;
+
+  if (!authorization) throw new AppError('JWT token is missing', 401);
+
+  const [, token] = authorization.split(' ');
+
+  const { secret } = authConfig.jwt;
+
+  try {
+    const decoded = verify(token, secret);
+
+    const { sub } = decoded as ITokenPayload;
+
+    request.user = { id: sub };
+
+    return next();
+  } catch (err) {
+    throw new AppError('Invalid JWT token', 401);
+  }
+}
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import UsersController from '../controllers/UsersController';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
 usersRouter.post('/', usersController.create);
+usersRouter.get('/', ensureAuthenticated, (request, response) => {
+  return response.json({ message: 'Authenticated!' });
+});
 
 export default usersRouter;
