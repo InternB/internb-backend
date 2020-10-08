2b76eb32e90a2fa97c37ab35f3c76f8ffe839631
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-10-08 15:03:02 -0300

    PORT from .env added

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -37,5 +37,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log('Server started on port 3333! 🚀 ');
+  console.log(`Server started on port ${process.env.PORT || 3333}! 🚀 `);
 });
