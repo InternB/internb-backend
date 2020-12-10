7759dd1385bc8184f45f32fea852314016ef9161
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2020-12-09 23:05:56 -0300

    Provedor de uploads removido

--- src/shared/container/providers/StorageProvider/model/IStorageProvider.ts
+++
@@ -1,7 +0,0 @@
-export default interface IStorageProvider {
-  saveFile(file: string): Promise<string>;
-  saveFiles(files: string[]): Promise<string[]>;
-  deleteFile(file: string): Promise<void>;
-  deleteFiles(files: string[]): Promise<void>;
-  deleteTmpFiles(files: string[]): Promise<void>;
-}
